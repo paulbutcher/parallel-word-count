d45984283566fdd13750e28913c5b68ddd518d75
Paul Butcher <paul@paulbutcher.com>
2013-09-28 22:22:27 +0100

    add -server to jvm-opts

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]]
   :main wordcount.core
-  :jvm-opts ["-Xms4G" "-Xmx4G" "-XX:NewRatio=8"])
+  :jvm-opts ^:replace ["-Xms4G" "-Xmx4G" "-XX:NewRatio=8" "-server"])
